d85cee3008d7d33dfbdef93c94b530a607c1dc69
Filippo Pracucci - filippo.pracucci@studio.unibo.it <filippo.pracucci@studio.unibo.it>
2022-11-15 17:14:48 +0100

    Just few lines with some errors

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Serializable;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public class Controller implements Serializable {
 
+    private File currentFile;
+
+    public Controller() {
+        this.currentFile = System.getProperty("user.home") + 
+            System.getProperty("file.separator") + "output.txt";
+    }
+
+    public void setCurrentFile(final File file) {
+        this.currentFile = file;
+    }
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public String getCurrentPath() {
+        return getCurrentFile().getAbsolutePath();
+    }
+
+    public void writeOnFile(final String s) throws IOException {
+        try (final BufferedWriter writer = new BufferedWriter(new FileWriter(getCurrentPath()))) {
+            writer.write(s);
+            writer.newLine();
+        }
+    }
 }
