a7762858a2c542ecf278e459e7ac991edb0a0acb
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2022-11-17 18:19:54 +0100

    First running implementation of Controller and SimpleGUI

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -2,8 +2,9 @@ package it.unibo.mvc;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.io.Serializable;
 
 /**
@@ -11,33 +12,50 @@ import java.io.Serializable;
  */
 public class Controller implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private File currentFile;
     private String currentPath;
-
+    /**
+     * Creates a new Controller.
+     */
     public Controller() {
-        this.currentPath = System.getProperty("user.home") + 
-            System.getProperty("file.separator") + "output.txt";
+        this.currentPath = System.getProperty("user.home")
+        + System.getProperty("file.separator") + "output.txt";
     }
-
-    public void setCurrentFile(final File file) {
+    /**
+     * @param file
+     * set the file as the current one.
+     */
+    public final void setCurrentFile(final File file) {
         this.currentFile = file;
         this.currentPath = file.getPath();
     }
-
-    public File getCurrentFile() {
+    /**
+     * @return the current file.
+     */
+    public final File getCurrentFile() {
         return this.currentFile;
     }
-
-    public String getCurrentPath() {
+    /**
+     * @return the current path.
+     */
+    public final String getCurrentPath() {
         return this.currentPath;
     }
-
-    public void writeOnFile(final String s) throws IOException {
-        try (final BufferedWriter writer = new BufferedWriter(new FileWriter(getCurrentPath()))) {
+    /**
+     * @param s that's a string.
+     * write on a file the string.
+     * @throws IOException
+     */
+    public final void writeOnFile(final String s) throws IOException {
+        try (
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(
+                            new FileOutputStream(getCurrentPath()), "UTF-8"))
+        ) {
             writer.write(s);
             writer.newLine();
-        } catch (IOException e) {
-            throw new IOException("IOException: " + e);
         }
     }
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,54 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private static final String TITLE = "My first java graphical interface";
+    private static final int PROPORTION = 3;
 
+    private final JFrame frame = new JFrame(TITLE);
+
+    /**
+     * Creates a new SimpleGUI.
+     */
+    public SimpleGUI() {
+        final JPanel panel = new JPanel(new BorderLayout());
+        frame.setContentPane(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JTextArea area = new JTextArea();
+        panel.add(area, BorderLayout.CENTER);
+        final JButton save = new JButton("Save");
+        panel.add(save, BorderLayout.SOUTH);
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    final Controller c = new Controller();
+                    c.writeOnFile(area.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "Error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace(); //NOPMD: allowed because this is an exercise
+                }
+            }
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int screenWidth = (int) screen.getWidth();
+        final int screenHeight = (int) screen.getHeight();
+        frame.setSize(screenWidth / PROPORTION, screenHeight / PROPORTION);
+
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+    }
+
+    /**
+     * Launches the application.
+     *
+     * @param args ignored
+     */
+    public static void main(final String[] args) {
+        new SimpleGUI().display();
+    }
 }
