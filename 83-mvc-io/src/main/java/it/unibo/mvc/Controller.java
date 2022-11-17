d48534d35399113b4739a617615076f41f7e76a2
Filippo Pracucci <filippo.pracucci@studio.unibo.it>
2022-11-17 23:06:52 +0100

    Implementation of interface Controller and class SimpleController

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,32 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
  *
  */
 public interface Controller {
 
+    /**
+     * 
+     * @param s is the next string to print
+     */
+    void setNextString(String s);
+
+    /**
+     * 
+     * @return the next string to print
+     */
+    String getNextString();
+
+    /**
+     * 
+     * @return the history of the printed strings as list of strings
+     */
+    List<String> getHistory();
+
+    /**
+     * 
+     */
+    void printCurrentString();
 }
--- 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,40 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+
+    private final List<String> list = new LinkedList<>();
+    private String nexString;
+
+    @Override
+    public void setNextString(final String s) {
+        this.nexString = Objects.requireNonNull(nexString, "Null value is not acceptable.");
+    }
+
+    @Override
+    public String getNextString() {
+        return this.nexString;
+    }
+
+    @Override
+    public List<String> getHistory() {
+        return this.list;
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (this.nexString == null) {
+            throw new IllegalStateException("Current string is unset.");
+        }
+        this.list.add(this.nexString);
+        System.out.println(this.nexString); //NOPMD: println allowed because is an exercise
+    }
 
 }
